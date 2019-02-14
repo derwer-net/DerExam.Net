276fe9c462fb0a85faad8de42fc6161df381f6df
raphaelli <raphael_li@live.com>
2019-02-14 15:41:51 +0800

    init with Identity

---
+++ DerExam.Net/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DerExam.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
