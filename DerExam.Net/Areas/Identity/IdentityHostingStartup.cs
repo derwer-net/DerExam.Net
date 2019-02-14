09cf958ff2276f4fc2cbc82723b947c5a3a63d25
raphaelli <raphael_li@live.com>
2019-02-14 17:28:15 +0800

    为项目添加基架- 显示Area中的 页面

---
+++ DerExam.Net/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DerExam.Net.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DerExam.Net.Areas.Identity.IdentityHostingStartup))]
+namespace DerExam.Net.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
