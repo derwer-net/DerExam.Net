be7981221e5932a6ee5a84585768764735c920e4
raphaelli <raphael_li@live.com>
2019-02-21 00:05:48 +0800

    add

---
+++ DerExam.Net/Models/UserExt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DerExam.Net.Models
+{
+    public class UserExt
+    {
+        public string ClassId { get; set; }
+        public string CourseId { get; set; }
+
+    }
+}
