9e6740b367e92b122c70fa411f597a43779df5e1
raphaelli <raphael_li@live.com>
2019-02-24 19:13:49 +0800

    efcore migration commit

--- DerExam.Net/Migrations/20190224110705_rebuild.Designer.cs
+++ DerExam.Net/Migrations/20190224110705_rebuild.Designer.cs
@@ -4,14 +4,16 @@ using DerExam.Net.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DerExam.Net.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190224110705_rebuild")]
+    partial class rebuild
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
